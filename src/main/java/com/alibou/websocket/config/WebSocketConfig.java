40c7a23019b7d8eb1df16c3d04bd3d0295e7d466
HimanshuSingour <singourhimanshu3@gmail.com>
2024-05-17 22:27:01 +0530

    Improvemdnt

--- src/main/java/com/alibou/websocket/config/WebSocketConfig.java
+++ src/main/java/com/alibou/websocket/config/WebSocketConfig.java
@@ -12,7 +12,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("http://localhost:8080", "https://546a-103-66-8-74.ngrok-free.app")
+                .withSockJS();
     }
 
     @Override
